7c499778701f857d4632b869a2d53a3c2d8ee19a
Adunwit Tiampae <atiampae@z3t6c1.42bangkok.com>
2022-10-21 13:52:41 +0700

    third day

--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: atiampae <atiampae@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/14 22:22:00 by atiampae          #+#    #+#             */
-/*   Updated: 2022/10/19 19:43:34 by atiampae         ###   ########.fr       */
+/*   Updated: 2022/10/21 13:46:35 by atiampae         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,15 +15,18 @@
 # include <stdlib.h>
 # include <unistd.h>
 
-int ft_isalpha(int c);
-int	ft_isdigit(int c);
-int	ft_isalnum(int c);
-int	ft_isascii(int c);
-int ft_isprint(int c);
-int ft_toupper(int c);
-int ft_tolower(int c);
+int		ft_isalpha(int c);
+int		ft_isdigit(int c);
+int		ft_isalnum(int c);
+int		ft_isascii(int c);
+int		ft_isprint(int c);
+int		ft_toupper(int c);
+int		ft_tolower(int c);
 size_t	ft_strlen(const char *s);
-char *ft_strchr(const char *s, int c);
-char *ft_strrchr(const char *s, int c);
+char	*ft_strchr(const char *s, int c);
+char	*ft_strrchr(const char *s, int c);
+void	*ft_memset(void *b, int c, size_t len);
+void	ft_bzero(void *s, size_t n);
+void	*ft_calloc(size_t count, size_t size);
 
 #endif
